ee0858f99fa2eb2136a78a0c97ce76caa50d533a
encetroc <g6handi@gmail.com>
2020-10-02 11:20:21 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { EMPTY, from, fromEvent, interval, NEVER, of, throwError, timer } from 'rxjs';
+import { BehaviorSubject, EMPTY, from, fromEvent, interval, NEVER, Observable, of, ReplaySubject, Subject, throwError, timer } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 import { filter, take } from 'rxjs/operators';
 
-// simple observable
+//simple observable
 const btnStart = document.getElementById('btnStart');
 const btnStop = document.getElementById('btnStop');
 const result = document.getElementById('result');
@@ -20,7 +21,7 @@ btnStart.addEventListener('click', () => {
   btnStop.addEventListener('click', () => subscription.unsubscribe())
 });
 
-// creating observables using of()
+//creating observables using of()
 const btnStart_of = document.getElementById('btnStart_of');
 const result_of = document.getElementById('result_of');
 
@@ -34,7 +35,7 @@ btnStart_of.addEventListener('click', () => {
   })
 })
 
-// creating subscriptions
+//creating subscriptions
 const btnStart_subscription = document.getElementById('btnStart_subscription');
 const result_subscription = document.getElementById('result_subscription');
 
@@ -58,7 +59,7 @@ btnStart_subscription.addEventListener('click', () => {
   time$.subscribe(observer);
 })
 
-// creating observables from dom events
+//creating observables from dom events
 const allButtons = document.querySelectorAll('button');
 const result_fromEvent = document.getElementById('result_fromEvent');
 const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -75,7 +76,7 @@ fromEvent(canvas, 'mousemove').subscribe((e: MouseEvent) => {
   ctx.fillRect(x, y, 2, 2);
 });
 
-// creating observables using timer
+//creating observables using timer
 const btnStart_timer = document.getElementById('btnStart_timer');
 const btnStop_timer = document.getElementById('btnStop_timer');
 const result_timer_date = document.getElementById('result_timer_date');
@@ -86,7 +87,7 @@ btnStart_timer.addEventListener('click', () => {
   const startTime = new Date();
   startTime.setSeconds(startTime.getSeconds() + 3);
   const sub1 = timer(startTime, 1000).subscribe(e => (result_timer_date.textContent = e.toString()));
-  // normal timer, starts after 3 second
+  //normal timer, starts after 3 second
   const sub2 = timer(5000, 1000).subscribe(e => (result_timer_normal.textContent = e.toString()));
 
   btnStop_timer.addEventListener('click', () => {
@@ -108,3 +109,113 @@ btnStart_from.addEventListener('click', () => {
   from(p).subscribe(e => result_from_promise.textContent = e.toString())
 })
 
+//using ajax
+const btnFetch_ajax = document .getElementById('btnFetch_ajax')
+const result_ajax = document .getElementById('result_ajax')
+const url = `https://api.chucknorris.io/jokes/random`;
+
+btnFetch_ajax.addEventListener('click', () => {
+  ajax.getJSON(url).subscribe(json => result_ajax.textContent = json.value);
+})
+
+//custom observables
+const btnStart_custom = document.getElementById('btnStart_custom')
+const btnStop_custom = document.getElementById('btnStop_custom')
+const result_custom = document.getElementById('result_custom')
+
+function time(ms) {
+  return Observable.create(subscriber => {
+    const handle = setInterval(() => {
+      subscriber.next(new Date().toLocaleTimeString());
+    }, ms);
+    return () => clearInterval(handle);
+  });
+}
+
+btnStart_custom.addEventListener('click', () => {
+  const subscription = time(1000).subscribe(time => result_custom.textContent = time);
+
+  btnStop_custom.addEventListener('click', () => subscription.unsubscribe())
+})
+
+//subject
+const btnStart_subject = document.getElementById('btnStart_subject')
+const result_replay_subject = document.getElementById('result_replay_subject')
+const result_behaviour_subject = document.getElementById('result_behaviour_subject')
+const result_subject = document.getElementById('result_subject')
+
+btnStart_subject.addEventListener('click', () => {
+
+  // replay subject
+  const replaySubject = new ReplaySubject();
+  //const replaySubject = new ReplaySubject(3);
+  replaySubject.next('zero')
+  replaySubject.next('one')
+  replaySubject.next('two')
+  replaySubject.subscribe(e => result_replay_subject.textContent = result_replay_subject.textContent + ' | ' + e)
+  replaySubject.next('three')
+  replaySubject.next('four')
+
+  // behaviour subject
+  const behaviorSubject = new BehaviorSubject('Start');
+  behaviorSubject.next('zero')
+  behaviorSubject.next('one')
+  behaviorSubject.next('two')
+  behaviorSubject.subscribe(e => result_behaviour_subject.textContent = result_behaviour_subject.textContent + ' | ' + e)
+  behaviorSubject.next('three')
+  behaviorSubject.next('four')
+
+  // normal subject
+  const subject = new Subject();
+  subject.next('zero')
+  subject.next('one')
+  subject.next('two')
+  subject.subscribe(e => result_subject.textContent = result_subject.textContent + ' | ' + e)
+  subject.next('three')
+  subject.next('four')
+})
+
+//cold vs hot observables
+const btnStart_cold = document.getElementById('btnStart_cold')
+const btnStop_cold = document.getElementById('btnStop_cold')
+const result_cold = document.getElementById('result_cold')
+const btnStart_hot = document.getElementById('btnStart_hot')
+const btnStop_hot = document.getElementById('btnStop_hot')
+const result_hot = document.getElementById('result_hot')
+
+function coldObservable() {
+  return Observable.create(subscriber => {
+    let count = 0;
+    const handle = setInterval(() => {
+      const value = count++;
+      subscriber.next(value);
+    }, 100);
+
+    return () => clearInterval(handle);
+  });
+}
+
+function hotObservable() {
+  const subject = new Subject();
+  let count = 0;
+  setInterval(() => {
+    const value = count++;
+    subject.next(value);
+  }, 100);
+
+  return subject;
+}
+
+const cold = coldObservable();
+btnStart_cold.addEventListener('click', () => {
+  const coldSub = cold.subscribe(time => (result_cold.textContent = time));
+
+  btnStop_cold.addEventListener('click', () => coldSub.unsubscribe())
+});
+
+const hot = hotObservable();
+btnStart_hot.addEventListener('click', () => {
+  const hotSub = hot.subscribe(time => (result_hot.textContent = time));
+
+  btnStop_hot.addEventListener('click', () => hotSub.unsubscribe())
+});
